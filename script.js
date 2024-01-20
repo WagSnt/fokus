1c804b54baff2093ac47ab766a7b6995df21b5dd
Wagner Santos <114152919+WagSnt@users.noreply.github.com>
2024-01-19 23:10:37 -0300

    manipulando imagens

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ const html = document.querySelector('html')
 const focoBtn = document.querySelector('.app__card-button--foco')
 const curtoBtn = document.querySelector('.app__card-button--curto')
 const longoBtn = document.querySelector('.app__card-button--longo')
+const imgBtn = document.querySelector('.app__image')
 
 focoBtn.addEventListener('click', () => {
-  html.setAttribute('data-contexto', 'foco')
+  alterarContexto('foco')
 })
 
 curtoBtn.addEventListener('click', () => {
-  html.setAttribute('data-contexto', 'descanso-curto')
+  alterarContexto('descanso-curto')
 })
 
 longoBtn.addEventListener('click', () => {
-  html.setAttribute('data-contexto', 'descanso-longo')
+  alterarContexto('descanso-longo')
 })
+
+function alterarContexto(contexto) {
+  html.setAttribute('data-contexto', contexto)
+  imgBtn.setAttribute('src', `/imagens/${contexto}.png`)
+}
